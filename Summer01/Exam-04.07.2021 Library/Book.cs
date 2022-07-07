0dcdaeeb29a9c846ed90cd61c0877dfdd716ff57
salihe <salihe.bodeva10@gmail.com>
2022-07-07 17:20:10 +0300

    Day001

---
+++ Summer01/Exam-04.07.2021 Library/Book.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+    public class Book
+    {
+
+        private string bookId;
+        private Book next;
+
+        public Book(string bookId)
+        {
+            this.bookId = bookId;
+        }
+
+        public string BookId
+        {
+            get { return bookId; }
+            set { bookId = value; }
+        }
+
+        public Book Next
+        {
+            get { return next; }
+            set { next = value; }
+        }
+
+        public override string ToString()
+        {
+            return $"Book {this.BookId}";
+        }
+    }
+
