3ef6706efca477058f4f50cd33e3c70a0f9baa54
salihe <salihe.bodeva10@gmail.com>
2022-07-09 16:11:00 +0300

    ex

---
+++ OOP Basics/Ex01-SJ ReversedString/Program.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+string word = Console.ReadLine();
+Stack<char> revefrsing = new Stack<char>();
+foreach (var item in word)
+{
+    revefrsing.Push(item);
+}
+Console.WriteLine(string.Join("",revefrsing));
