a67b2d4a765621bf1073caf4d38fce9c275015cf
salihe <salihe.bodeva10@gmail.com>
2022-07-10 19:56:42 +0300

    10.07.2022

--- OOP Basics/Ex02-SJ Stack Sum/Program.cs
+++ OOP Basics/Ex02-SJ Stack Sum/Program.cs
@@ -1,1 +1,37 @@
 ﻿
+List<int> inputs = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+
+
+Stack<int> ints = new Stack<int>();
+foreach (var item in inputs)
+{
+    ints.Push(item);
+}
+while (true)
+{
+    List<string> cmd = Console.ReadLine().ToLower().Split(' ').ToList();
+    if (cmd[0]=="add")
+    {
+        int a = int.Parse(cmd[1]);
+        int b = int.Parse(cmd[2]); 
+        ints.Push(a);
+        ints.Push(b);
+    }
+    if (cmd[0] == "remove")
+    {
+        int r = int.Parse(cmd[1]);
+        if (ints.Count>=r)
+        {
+            for (int i = 0; i < r; i++)
+            {
+                ints.Pop();
+            }
+        }
+    }
+    if (cmd[0] == "end")
+    {
+        int sum = ints.Sum();
+        Console.WriteLine($"Sum:{sum}");
+        break;
+    }
+}
---
+++ OOP Basics/Ex4-SJ- Print Even Numbers/Program.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+int[] input = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+Queue<int> output = new Queue<int>();
+foreach (var item in input)
+{
+    if (item%2==0)
+    {
+        output.Enqueue(item);
+    }
+}
+Console.WriteLine(String.Join(", ",output));
