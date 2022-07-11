13715749bceee0f366a47727e622fd92543ef7b1
salihe <salihe.bodeva10@gmail.com>
2022-07-11 15:30:14 +0300

    11.07.2022

---
+++ OOP Basics/Ex05-SJ-Supermarket-Kaufland/Program.cs
@@ -0,0 +1,27 @@
+﻿Queue<string> customers = new Queue<string>();
+Queue<string> output = new Queue<string>();
+
+while (true)
+{
+    string cmd = Console.ReadLine();
+    if (cmd != "Paid")
+    {
+        customers.Enqueue(cmd);
+    }
+    if (cmd=="Paid")
+    {
+        foreach (var item in customers)
+        {
+            output.Enqueue(item);
+        }
+        customers.Clear();
+    }
+    if (cmd=="End")
+    {
+        foreach (var item in output)
+        {
+            Console.WriteLine(item);
+        }
+        Console.WriteLine($"{customers.Count-1} people remaining");
+    }
+}
---
+++ OOP Basics/Ex06-SJ-Hot Potato/Program.cs
@@ -0,0 +1,27 @@
+﻿int n = int.Parse(Console.ReadLine());
+Queue<string> cars = new Queue<string>();
+Queue<string> carsOutput = new Queue<string>();
+while (true)
+{
+    string cmd = Console.ReadLine();
+    if (cmd == "green")
+    {
+        foreach (var item in cars)
+        {
+            if (!carsOutput.Contains(item))
+            {
+                carsOutput.Enqueue(item);
+            }           
+        }
+    }
+    cars.Enqueue(cmd);
+    if (cmd=="end")
+    {
+        foreach (var item in carsOutput)
+        {
+            Console.WriteLine($"{item} passed!");
+        }
+        Console.WriteLine($"{carsOutput.Count} cars passed the crossroads.");
+        break;
+    }
+}
