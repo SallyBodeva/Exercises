c71f80408b3088acd3251a48822c936551dcf7f3
salihe <salihe.bodeva10@gmail.com>
2022-07-16 23:26:37 +0300

    17.06.2022

---
+++ OOP Basics/Ex0.02-SJ-Fast food/Program.cs
@@ -0,0 +1,42 @@
+﻿int quantityFood = int.Parse(Console.ReadLine());
+List<int> ints = Console.ReadLine().Split(" ").Select(int.Parse).ToList();
+Queue<int> Orders = new Queue<int>();
+Queue<int> leftOrders = new Queue<int>();
+
+foreach (var item in ints)
+{
+    Orders.Enqueue(item);
+}
+int max = int.MinValue;
+foreach (var item in Orders)
+{
+    if (item>max)
+    {
+        max = item;
+    }
+}
+Console.WriteLine(max);
+foreach (var item in Orders)
+{
+    if (item<quantityFood)
+    {
+        Orders.Dequeue();
+        quantityFood -= item;
+    }
+    else
+    {
+        leftOrders.Enqueue(item);
+    }
+}
+if (quantityFood>=0)
+{
+    Console.WriteLine($"Orders complete");
+}
+else
+{
+    foreach (var item in leftOrders)
+    {
+        Console.WriteLine($"Orders left: {item}");
+    }
+}
+
